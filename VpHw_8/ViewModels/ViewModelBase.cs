74040a3cf6cd7f93bdbab7a1fd7c714b020b13eb
nikoros2d <99119715+nikoros2d@users.noreply.github.com>
2023-04-23 22:52:01 +0700

    clear old files

--- VpHw_8/ViewModels/ViewModelBase.cs
+++
@@ -1,6 +0,0 @@
-﻿using ReactiveUI;
-
-namespace VpHw_8.ViewModels {
-    public class ViewModelBase: ReactiveObject {
-    }
-}
--- VpHw_8/Views/AddShape.axaml.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Controls;
-
-namespace VpHw_8.Views {
-    public partial class AddShape: Window {
-        public AddShape() {
-            InitializeComponent();
-        }
-    }
-}
